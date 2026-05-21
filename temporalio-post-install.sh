6dd7e91f862ac36926f509c96cc63f645680533c
Karoly Negyesi <chx1975@gmail.com>
2026-05-21 19:00:31 +0200

    do add additional hostname but with the right priority

--- temporalio-post-install.sh
+++ temporalio-post-install.sh
@@ -7,6 +7,8 @@ TLD=$(awk '/^project_tld:/{print $2}' config.yaml)
 TLD=${TLD:-ddev.site}
 HOST="temporalio-ui.$NAME.$TLD"
 
+docker run --rm -v $(pwd):/data -u $(id -u):$(id -g) chx1975/ddev-install-additional-hostname "temporalio-ui.$NAME"
+
 sed "s/NAME_REPLACE_THIS/$NAME/" traefik/config/temporalio-ui.yaml > traefik/config/temporalio-ui.tmp
 sed "s/HOST_REPLACE_THIS/$HOST/" traefik/config/temporalio-ui.tmp > traefik/config/temporalio-ui.yaml
 rm traefik/config/temporalio-ui.tmp
