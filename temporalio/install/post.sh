8027eefbc5903dbc256b2a8c1c9c30b5df6fa58c
chx <chx1975@gmail.com>
2026-05-27 12:17:25 +0200

    use yq instead

--- temporalio/install/post.sh
+++ temporalio/install/post.sh
@@ -6,11 +6,11 @@ TLD=$(awk '/^project_tld:/{print $2}' config.yaml)
 TLD=${TLD:-ddev.site}
 HOST="temporalio-ui.$NAME.$TLD"
 
-docker run --rm -v $(pwd):/data -u $(id -u):$(id -g) \
-  $(docker build -q -f ./temporalio/install/Dockerfile .) /data/config.yaml \
-  --append-unique .additional_hostnames "temporalio-ui.$NAME" \
-  --append-unique .webimage_extra_packages 'php${DDEV_PHP_VERSION}-grpc' \
-  --flow .webimage_extra_packages
+docker run --rm -v $(pwd):/data -u $(id -u):$(id -g) -e "HOST=temporalio-ui.$NAME" -e GRPC='php${DDEV_PHP_VERSION}-grpc' \
+  ddev/ddev-utilities yq -i '
+    ((.additional_hostnames | select([.[] | . != strenv(HOST)] | all)) += [strenv(HOST)]) | 
+    ((.webimage_extra_packages | select([.[] | . != strenv(GRPC)] | all)) += [strenv(GRPC)]) | 
+    .webimage_extra_packages style = "flow"' /data/config.yaml
 
 sed "s/NAME_REPLACE_THIS/$NAME/" traefik/config/temporalio-ui.yaml > traefik/config/temporalio-ui.tmp
 sed "s/HOST_REPLACE_THIS/$HOST/" traefik/config/temporalio-ui.tmp > traefik/config/temporalio-ui.yaml
