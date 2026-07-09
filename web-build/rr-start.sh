e1dd6b1fb667fa21cb51e8ce19ba7ecd623bf117
Charlie Negyesi <chx1975@gmail.com>
2026-07-09 22:09:17 +0200

    also check whether the worker command exists

--- web-build/rr-start.sh
+++ web-build/rr-start.sh
@@ -5,7 +5,7 @@
 while true; do
     line="$(composer show drupal/temporal --path 2>/dev/null)"
     install_path="${line#drupal/temporal }"
-    if [[ -n "$install_path" ]]; then
+    if [[ -n "$install_path" ]] && drush list 2>/dev/null | grep -q temporal:worker; then
         exec /usr/local/bin/rr serve -c "$install_path/.rr.yaml"
     fi
     sleep 2
