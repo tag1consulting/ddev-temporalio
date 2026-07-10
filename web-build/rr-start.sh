f70d33c417122d2f6d62242ac2de794ccedd62ee
Charlie Negyesi <chx1975@gmail.com>
2026-07-10 02:37:55 +0200

    make it work with ddev contrib

--- web-build/rr-start.sh
+++ web-build/rr-start.sh
@@ -3,8 +3,12 @@
 #ddev-silent-no-warn
 
 while true; do
-    line="$(composer show drupal/temporal --path 2>/dev/null)"
-    install_path="${line#drupal/temporal }"
+    if [[ -f "./.rr.yaml" ]]; then
+        install_path="."
+    else
+        line="$(composer show drupal/temporal --path 2>/dev/null)"
+        install_path="${line#drupal/temporal }"
+    fi
     if [[ -n "$install_path" ]] && drush list 2>/dev/null | grep -q temporal:worker; then
         exec /usr/local/bin/rr serve -c "$install_path/.rr.yaml"
     fi
