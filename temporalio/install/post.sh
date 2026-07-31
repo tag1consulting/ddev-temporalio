44a33ba469404447e62c738407bc1b95f8021ba9
Charlie Negyesi <chx1975@gmail.com>
2026-07-31 11:01:35 +0200

    make the roadrunner image configurable

--- temporalio/install/post.sh
+++ temporalio/install/post.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-NAME=$(awk '/^name:/{print $2}' config.yaml)
-perl -pi -e "s/NAME_REPLACE_THIS/$NAME/" config.temporalio.yaml
+NAME=$(awk '/^name:/{print $2}' config.yaml) \
+  perl -pi -e 's#NAME_REPLACE_THIS#$ENV{NAME}#' config.temporalio.yaml
+TEMPORALIO_ROADRUNNER_IMAGE=$(awk '/^temporalio_roadrunner_image:/{print $2}' config.temporalio.user.yaml) \
+  perl -pi -e 's#TEMPORALIO_ROADRUNNER_IMAGE_REPLACE_THIS#$ENV{TEMPORALIO_ROADRUNNER_IMAGE}#' web-build/Dockerfile.temporalio
